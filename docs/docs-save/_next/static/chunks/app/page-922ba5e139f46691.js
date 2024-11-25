b112cbed3001b91723c3494457557ac9d363d129
Dillon Laird <dillonalaird@gmail.com>
2024-11-24 16:05:33 -0800

    add static webpage to deploy branch

---
+++ docs/docs-save/_next/static/chunks/app/page-922ba5e139f46691.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{295:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,371,23)),Promise.resolve().then(n.t.bind(n,6410,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[822,123,911,744],()=>s(295)),_N_E=e.O()}]);
