065bb5d7178b872b3333341dc307cef840ca2cd7
Dillon Laird <dillonalaird@gmail.com>
2024-11-26 17:56:16 -0800

    use jpg image

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -5,7 +5,7 @@ export function Intro() {
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <div className="flex items-center gap-8">
         <Image
-          src="/assets/self.png"
+          src="/assets/self.jpg"
           alt="Dillon Laird"
           className="ml-8 w-24 h-24 md:w-80 md:h-80 rounded-full object-cover border-4 border-gray-200"
           width={512}
